371fdffdf1a332bec91e11fccbdc3377ee24b145
Sofya Murzakova <murz.sophie@gmail.com>
2023-11-27 01:13:09 +0100

    changed img routes in fe

--- sd-frontend-app/src/api/images.js
+++ sd-frontend-app/src/api/images.js
@@ -35,7 +35,7 @@ export const getMyImages = async () => {
 };
 
 // create new image
-export const generateImage = async (subject, artDirection, artist) => {
+export const generateTxt2Image = async (subject, artDirection, artist) => {
     try {
         const response = await api.post("images/txt2img", {
             subject,
@@ -46,6 +46,77 @@ export const generateImage = async (subject, artDirection, artist) => {
         return response;
     } catch (error) {
         console.log("Error by image generating: ", error.response ? error.response.data.error : error);
+        throw error;
+    }
+};
+
+export const generateTxt2ImageSDAPI = async (subject, artDirection, artist) => {
+    try {
+        const response = await api.post("images/sd-api/txt2img", {
+            subject,
+            artDirection,
+            artist
+        });
+        console.log(response.data);
+        return response;
+    } catch (error) {
+        console.log("Error by image generating: ", error.response ? error.response.data.error : error);
+        throw error;
+    }
+};
+
+export const generateImg2Image = async (file, subject, artDirection, artist) => {
+    try {
+        const data = new FormData();
+        data.append('file', file);
+        data.append('subject', subject);
+        data.append('artDirection', artDirection);
+        data.append('artist', artist);
+
+        const requestOptions = {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        }
+
+        const response = await api.post("images/img2img", data, requestOptions);
+        console.log(response.data);
+        return response;
+    } catch (error) {
+        console.log("Error by image generating: ", error.response ? error.response.data.error : error);
+        throw error;
+    }
+};
+
+export const generateImg2ImageSDAPI = async (file, subject, artDirection, artist) => {
+    try {
+        const data = new FormData();
+        data.append('file', file);
+        data.append('subject', subject);
+        data.append('artDirection', artDirection);
+        data.append('artist', artist);
+
+        const requestOptions = {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        }
+
+        const response = await api.post("images/sd-api/img2img", data, requestOptions);
+        console.log(response.data);
+        return response;
+    } catch (error) {
+        console.log("Error by image generating: ", error.response ? error.response.data.error : error);
+        throw error;
+    }
+};
+
+// delete genereted image
+export const deleteGeneratedImageById = async (generationID) => {
+    try {
+        const response = await api.delete(`images/generations/${generationID}`);
+        return response;
+    } catch (error) {
         throw error;
     }
 };
