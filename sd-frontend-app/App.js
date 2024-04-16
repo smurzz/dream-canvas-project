6f7dd65cae1f8e88958dc5f780141c3841c66b94
Sofya Murzakova <murz.sophie@gmail.com>
2024-04-16 02:49:56 +0200

    added errorWarn remover

--- sd-frontend-app/App.js
+++ sd-frontend-app/App.js
@@ -10,6 +10,20 @@ import HomeBottomTabs from './src/components/HomeBottomTabs';
 
 const Stack = createStackNavigator();
 
+if (__DEV__) {
+  const ignoreWarns = ["VirtualizedLists should never be nested inside plain ScrollViews"];
+
+  const errorWarn = global.console.error;
+  global.console.error = (...arg) => {
+    for (const error of ignoreWarns) {
+      if (arg[0].startsWith(error)) {
+        return;
+      }
+    }
+    errorWarn(...arg);
+  };
+}
+
 export default function App() {
 
   const [tokenExpired, setTokenExpired] = useState(false);
